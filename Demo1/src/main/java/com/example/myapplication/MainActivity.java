616384488fdf2f70bc444d8965eb696aa97a7408
liuzhouliang <525688067@qq.com>
2019-02-01 18:53:10 +0800

    gradle

---
+++ Demo1/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @ describe 
+ * 
+ * @author lzl
+ *
+ * @ time 2019/2/1 18:51
+ * 
+ * @ param
+ * 
+ * @ return
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
